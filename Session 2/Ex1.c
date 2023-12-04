de1d076b8de0966967015db945f5e6feab7f2997
lhbtran2003 <lhbtran2003@gmail.com>
2023-12-04 15:52:57 +0700

    first commit

---
+++ Session 2/Ex1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+int main(){
+    //khai báo gán giá trị biến hình vuông
+    // tính chu vi và diện tích
+    int edge = 4;
+    printf("chu vi hình vuong laa: %d\n", 4*edge);
+    printf("dien tich hinh vuong la: %d\n", edge*edge);
+    //tính chu vi và diện tích tam giác
+    double a =2, b= 3, c= 4;
+    printf("chu vi tam giac la: %.2lf\n", a+b+c);
+    double p = (a + b +c) /2;
+    // square root: dùng căn bậc 2
+    double area = sqrt(p*(p-a)*(p-b)*(p-c));
+    printf("Dien tich tam giac la: %.2lf\n", area );
+
+    return 0;
+
+}
+
