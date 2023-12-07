2ea5c7677e37ea4609e3f580904455bf78383b09
lhbtran2003 <lhbtran2003@gmail.com>
2023-12-07 08:38:29 +0700

    change code

--- Session 4/Ex3.c
+++ Session 4/Ex3.c
@@ -9,6 +9,17 @@ int main()
     scanf("%f", &so_moi );
     float so_dien = so_moi - so_cu;
 
-    if
-
+    if (so_dien >= 0 && so_dien < 50)
+    {
+        printf("gia tien = 10.000");
+    } else if (so_dien >= 50 && so_dien <100)
+    { printf("gia tien = 15.000") ;
+    } else if (100 <= so_dien <150)
+    { printf("gia tien = 20.000");
+    } else if (150 <= so_dien < 200)
+    { printf("gia tien = 25.000");
+    } else if (200 <= so_dien)
+    { printf("gia tien = 30.000");
+    }
+    return 0;
 }
